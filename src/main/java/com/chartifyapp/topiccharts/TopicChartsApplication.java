cbadceb37c2204119e377eba2b3da661bc4905d1
liyi.54 <adeliyiseyi@gmail.com>
2020-03-05 00:10:52 +0100

    First commit

---
+++ src/main/java/com/chartifyapp/topiccharts/TopicChartsApplication.java
@@ -0,0 +1,13 @@
+package com.chartifyapp.topiccharts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicChartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicChartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chartifyapp/topiccharts/TopicChartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chartifyapp.topiccharts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicChartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
